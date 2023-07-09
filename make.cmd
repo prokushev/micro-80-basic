2d72ca3852d410b6ed6c3330c62760da3cdfdc36
prokushev <yuri.prokushev@gmail.com>
2023-07-09 13:07:59 +0700

    Added Radio-86RK 32kb version.

--- make.cmd
+++ make.cmd
@@ -4,6 +4,9 @@ p2bin basic80.p basic80.bin
 asw -lU -D RK86=1 basic80.asm > basic80-rk86.lst
 p2bin basic80.p basic80-rk86.bin
 
+asw -lU -D RK86=1 -D RK86_16=0 basic80.asm > basic80-rk86-32.lst
+p2bin basic80.p basic80-rk86-32.bin
+
 asw -lU -D UT88=1 basic80.asm > basic80-ut88.lst
 p2bin basic80.p basic80-ut88.bin
 
