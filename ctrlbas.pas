3c3a8eda57d24f3213a0c1b38c85aa808e0d07e7
prokushev <yuri.prokushev@gmail.com>
2023-07-11 16:24:21 +0700

    More checksums for easest control...

--- ctrlbas.pas
+++ ctrlbas.pas
@@ -31,11 +31,66 @@ $090, $038, $0D6, $0F4, $06A, $074, $0CB,
 $033, $093, $065, $070, $0BF, $0D6, $0A2,
 $0B4, $03D, $01A, $06A
 );
+// Опубликовано в журнале
 	servicesums: array of tsum = (
 		(s:$0000; e:$19FF; sum:$BDED),
 		(s:$1A00; e:$1CFF; sum:$802D),
 		(s:$0000; e:$1CFF; sum:$421A)
 	);
+// Сгенерировано заново, более подробно
+	servicesums2: array of tsum = (
+		(s:$1A00; e:$1A0F; sum:$4506),
+		(s:$1A10; e:$1A1F; sum:$354b),
+		(s:$1A20; e:$1A2F; sum:$7724),
+		(s:$1A30; e:$1A3F; sum:$C5BB),
+		(s:$1A40; e:$1A4F; sum:$F344),
+		(s:$1A50; e:$1A5F; sum:$F0B0),
+		(s:$1A60; e:$1A6F; sum:$5F67),
+		(s:$1A70; e:$1A7F; sum:$A9C1),
+		(s:$1A80; e:$1A8F; sum:$AFD0),
+		(s:$1A90; e:$1A9F; sum:$3C33),
+		(s:$1AA0; e:$1AAF; sum:$97D8),
+		(s:$1AB0; e:$1ABF; sum:$E8A9),
+		(s:$1AC0; e:$1ACF; sum:$06C9),
+		(s:$1AD0; e:$1ADF; sum:$37FE),
+		(s:$1AE0; e:$1AEF; sum:$F870),
+		(s:$1AF0; e:$1AFF; sum:$4347),
+
+		(s:$1B00; e:$1B0F; sum:$BABD),
+		(s:$1B10; e:$1B1F; sum:$BDD4),
+		(s:$1B20; e:$1B2F; sum:$0C22),
+		(s:$1B30; e:$1B3F; sum:$3E15),
+		(s:$1B40; e:$1B4F; sum:$ACA9),
+		(s:$1B50; e:$1B5F; sum:$6028),
+		(s:$1B60; e:$1B6F; sum:$EDAA),
+		(s:$1B70; e:$1B7F; sum:$6966),
+		(s:$1B80; e:$1B8F; sum:$DC8F),
+		(s:$1B90; e:$1B9F; sum:$7F29),
+		(s:$1BA0; e:$1BAF; sum:$69A8),
+		(s:$1BB0; e:$1BBF; sum:$310f),
+		(s:$1BC0; e:$1BCF; sum:$B79c),
+		(s:$1BD0; e:$1BDF; sum:$8786),
+		(s:$1BE0; e:$1BEF; sum:$8BB8),
+		(s:$1BF0; e:$1BFF; sum:$8097),
+
+		(s:$1C00; e:$1C0F; sum:$01D7),
+		(s:$1C10; e:$1C1F; sum:$6AE8),
+		(s:$1C20; e:$1C2F; sum:$FD13),
+		(s:$1C30; e:$1C3F; sum:$2516),
+		(s:$1C40; e:$1C4F; sum:$74A5),
+		(s:$1C50; e:$1C5F; sum:$29ED),
+		(s:$1C60; e:$1C6F; sum:$9991),
+		(s:$1C70; e:$1C7F; sum:$9515),
+		(s:$1C80; e:$1C8F; sum:$6DFD),
+		(s:$1C90; e:$1C9F; sum:$7412),
+		(s:$1CA0; e:$1CAF; sum:$DEF3),
+		(s:$1CB0; e:$1CBF; sum:$7629),
+		(s:$1CC0; e:$1CCF; sum:$E1F6),
+		(s:$1CD0; e:$1CDF; sum:$E1E0),
+		(s:$1CE0; e:$1CEF; sum:$4F54),
+		(s:$1CF0; e:$1CFF; sum:$E3E1)
+	);
+// Опубликовано в журнале
 	rk86sums: array of tsum = (
 		(s:$0000; e:$19FF; sum:$1242),
 		(s:$0000; e:$00ff; sum:$036f),
@@ -86,8 +141,13 @@ begin
 			i:=i+1;
 		end;
 		sum:=byte(sum);
-		write('Блок ', IntToHex(fsums[j].s,4), '-', IntToHex(fsums[j].e,4), ' Сумма ', IntToHex(byte(csum shr 8),2), IntToHex(sum,2),' ');
-		if (fsums[j].sum=sum+byte(csum shr 8) shl 8) then WriteLn('OK') else WriteLn('ОШИБКА');
+		
+		if not (fsums[j].sum=sum+byte(csum shr 8) shl 8) then
+		begin
+			write('Блок ', IntToHex(fsums[j].s,4), '-', IntToHex(fsums[j].e,4), ' Сумма ', IntToHex(byte(csum shr 8),2), IntToHex(sum,2),' ');
+			WriteLn('ОШИБКА');
+			Halt(255);
+		end;
 		b:=b+1;      
 		sum:=0;
 		csum:=0;
@@ -111,8 +171,12 @@ begin
 		i:=i+1;
 		if i=256 then
 		begin
-			write('Блок ', IntToHex(b,2),'00-', IntToHex(b,2), 'FF Сумма ', IntToHex(byte(sum),2),' ');
-			if micro80sums[b]=byte(sum) then WriteLn('OK') else WriteLn('ОШИБКА');
+			if not (micro80sums[b]=byte(sum)) then
+			begin
+				write('Блок ', IntToHex(b,2),'00-', IntToHex(b,2), 'FF Сумма ', IntToHex(byte(sum),2),' ');
+				WriteLn('ОШИБКА');
+				Halt(255);
+			end;
 			b:=b+1;      
 			sum:=0;
 			csum:=0;
@@ -128,4 +192,5 @@ begin
 	CheckMicro80File('basic80.bin');
 	CheckFile('basic80-rk86.bin', rk86sums);
 	CheckFile('basic80-rk86-service.bin', servicesums);
+	CheckFile('basic80-rk86-service.bin', servicesums2);
 end.
--- make.cmd
+++ make.cmd
@@ -1,77 +1,130 @@
-rem Ð¡Ñ‚Ð°Ð½Ð´Ð°Ñ€Ñ‚Ð½Ñ‹Ðµ Ð±ÐµÐ¹ÑÐ¸ÐºÐ¸
+@echo off
+rem ‘â ­¤ àâ­ë¥ ¡¥©á¨ª¨
 
 asw -lU basic80.asm > basic80.lst
-p2bin basic80.p basic80.bin
+if errorlevel 1 goto error
+ p2bin basic80.p basic80.bin
+if errorlevel 1 goto error
 
 asw -lU -D RAM=12 basic80.asm > basic80-12.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-12.bin
+if errorlevel 1 goto error
 
 asw -lU -D RAM=32 basic80.asm > basic80-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D RAM=48 basic80.asm > basic80-48.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-48.bin
+if errorlevel 1 goto error
 
 asw -lU -D RK86=1 basic80.asm > basic80-rk86.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-rk86.bin
+if errorlevel 1 goto error
 
 asw -lU -D RK86=1 -D RAM=32 basic80.asm > basic80-rk86-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-rk86-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D UT88=1 basic80.asm > basic80-ut88.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88.bin
+if errorlevel 1 goto error
 
 asw -lU -D UT88=1 -D RAM=12 basic80.asm > basic80-ut88-12.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-12.bin
+if errorlevel 1 goto error
 
 asw -lU -D UT88=1 -D RAM=32 basic80.asm > basic80-ut88-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D UT88=1 -D RAM=48 basic80.asm > basic80-ut88-48.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-48.bin
+if errorlevel 1 goto error
 
 asw -lU ctrlbas.asm > ctrlbas.lst
+if errorlevel 1 goto error
 p2bin ctrlbas.p ctrlbas.bin
+if errorlevel 1 goto error
 
-rem Ð‘ÐµÐ¹ÑÐ¸Ðº-Ð¡ÐµÑ€Ð²Ð¸Ñ
+rem ¥©á¨ª-‘¥à¢¨á
 
 asw -lU -D SERVICE=1 basic80.asm > basic80-service.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-service.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D RAM=12 basic80.asm > basic80-service-12.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-service-12.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D RAM=32 basic80.asm > basic80-service-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-service-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D RAM=48 basic80.asm > basic80-service-48.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-service-48.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D RK86=1 basic80.asm > basic80-rk86-service.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-rk86-service.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D RK86=1 -D RAM=32 basic80.asm > basic80-rk86-service-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-rk86-service-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D UT88=1 basic80.asm > basic80-ut88-service.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-service.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D UT88=1 -D RAM=12 basic80.asm > basic80-ut88-service-12.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-service-12.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D UT88=1 -D RAM=32 basic80.asm > basic80-ut88-service-32.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-service-32.bin
+if errorlevel 1 goto error
 
 asw -lU -D SERVICE=1 -D UT88=1 -D RAM=48 basic80.asm > basic80-ut88-service-48.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-ut88-service-48.bin
+if errorlevel 1 goto error
 
-rem ÐœÐ¾Ð¸ Ð²Ð°Ñ€Ð¸Ð°Ð½Ñ‚Ñ‹
+rem Œ®¨ ¢ à¨ ­âë
 
 asw -lU -D BASICNEW=1 basic80.asm > basic80-new.lst
+if errorlevel 1 goto error
 p2bin basic80.p basic80-new.bin
+if errorlevel 1 goto error
 
 bin2rk basic80.bin
 bin2rk ctrlbas.bin $2800
 
+fpc ctrlbas
 ctrlbas
 
+goto exit
+
+:error
+
+echo Žè¨¡ª  á¡®àª¨
+
+:exit
